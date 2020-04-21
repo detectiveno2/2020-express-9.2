2c0b2f723ee01bd475ec1ae5347101fdcd9e9cf7
Glitch (hello-express) <none>
2020-04-21 11:16:59 +0000

    📫🛫 Checkpoint ./server.js:15235691/9768 ./views/books/index.pug:15235691/30

--- server.js
+++ server.js
@@ -6,51 +6,63 @@
 const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 db.defaults({ books: [] }).write();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.get('/books', (req, res) => {
-  res.render('books/index', {
-    books: db.get('books').value(),
-  })
-})
-
-app.get('/books/create', (req, res) => {
-  res.render('books/create');
-})
-
-app.post('/books/create', (req, res) => {
-  req.body.id = shortid.generate();
-  db.get('books').push(req.body).write();
-  res.redirect('/books');
-})
-
-app.get('books/edit/:id', (req, res) => {
-  res.render('books/edit', {
-    id: req.params.id,
+app.get("/books", (req, res) => {
+  res.render("books/index", {
+    books: db.get("books").value()
   });
-})
+});
 
-app.post('books/edit', (req, res) => {
-  db.get('todos').find({id: req.body.id}).assign({title: req.body.title}).write();
-  res.redirect('')
-})
+app.get("/books/create", (req, res) => {
+  res.render("books/create");
+});
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
+app.post("/books/create", (req, res) => {
+  req.body.id = shortid.generate();
+  db.get("books")
+    .push(req.body)
+    .write();
+  res.redirect("/books");
+});
+
+app.get("/books/edit/:id", (req, res) => {
+  res.render("books/edit", {
+    id: req.params.id
+  });
+});
+
+app.post("/books/edit", (req, res) => {
+  db.get("books")
+    .find({ id: req.body.id })
+    .assign({ title: req.body.title })
+    .write();
+  res.redirect("/books");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/books/delete/:id", (req, res) => {
+  db.get("books")
+    .remove({ id: req.params.id })
+    .write();
+  res.redirect("/books");
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
